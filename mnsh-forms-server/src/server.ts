3a49c24265cce0b2160e1164f9d6668e6c018b23
Seif-Elmenshawy <seif.ahmed.3849@gmail.com>
2026-07-18 06:14:44 +0300

    edit the cors option so it allows the deployed client

--- mnsh-forms-server/src/server.ts
+++ mnsh-forms-server/src/server.ts
@@ -13,7 +13,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://mnsh-forms-omega.vercel.app"],
   credentials:true
 }))
 const port = process.env.PORT || 3000
